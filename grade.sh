2cce2d660d6987c1c1c441eba8c8d1b088f29902
Davidg8011 <147877398+Davidg8011@users.noreply.github.com>
2023-11-27 12:19:32 -0800

    11/27/23

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,7 +7,21 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+if [[ -f ./student-submission/ListExamples.java ]]
+then 
+    cp -r ./TestListExamples.java ./grading-area
+    cp -r ./student-submission/ListExamples.java ./grading-area
 
+    cd grading-area/
+    echo 'Finished cloning2'
+    javac -cp $CPATH *.java
+    echo 'Finished cloning54'
+    java -cp $CPATH org.junit.runner.JUnitCore TestListExample
+    echo 'Finished cloning3'
+else
+echo 'TestListExamples.java was not found'
+
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
